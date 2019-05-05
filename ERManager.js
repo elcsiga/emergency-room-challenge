6fb9218e0d90cc5ba47e324ab2eee2775ca3d595
Andras Kovacs <elcsiga@gmail.com>
2019-05-05 23:06:41 +0200

    it2

--- ERManager.js
+++ ERManager.js
@@ -9,35 +9,36 @@
 
 class ERManager {
 
-    //  For each game a new ParkingManager() object will be created.
+    //  For each game a new ERManager() object will be created.
     constructor() {
         console.log('Starting a new game...');
-        this.index = 0;
     }
 
-    //  selectRoom() will be called 100 times as the 100 rats arrives.
-    //  The function receives the type ot the car as a function parameter
-    //  ('employee', 'disabled' or 'manager').
-    //  You must send the car into one of the parking lots by returning a string
-    //  ('underground-garage'. 'reno-court' or 'open-area').
-    selectRoom(nextRat) {
+    //  selectRoom() will be called repetitively until there are rats in the hall.
+    //  The function receives an array which represents the rats sitting in the hall
+    //  e.g.: ['', '', '']
+    //  the first item represents the rat sitting on the chair 1, etc.
+    //
+    //  You must send ONE of the rats into ONE of the rooms by returning an object
+    //  e.h.: { rat: 2, room: 3 }
+    //  rat is the sit number the rat in the hall (
+    //
+    //  If you send a rat into a full room, the rat will remain in the hall
+    //
 
-        // As an example, here we select a random parking lot.
+
+    redirectRatToRoom(ratsInTheHall) {
+
+        // As an example, here we send a random rat to a random room:
         // You can do it better...
-        const roomId = [
-            'underground-garage',
-            'reno-court',
-            'open-area'
-        ][Math.floor(Math.random() * 3)];
+        const rat = Math.floor(Math.random() * 5) + 1; // 1..5
+        const room = Math.floor(Math.random() * 3) + 1; // 1..3
 
-        console.log(`#${this.index} ${nextRat} -> ${roomId}`);
-
-        this.index ++;
-        return roomId;
+        return { rat, room };
     }
 }
 
-// makong ParkingManager class gobally accessible
+// making ERManager class globally accessible
 window.ERManager = ERManager;
 
 
--- game/index.js
+++ game/index.js
@@ -1,9 +1,7 @@
-
-
 (ERManager => {
 
-	let SILENT_MODE = false;
-	
+    let SILENT_MODE = false;
+
     const RAT_WIDTH = parseInt(getComputedStyle(document.body).getPropertyValue('--rat-width'));
     const RAT_HEIGHT = parseInt(getComputedStyle(document.body).getPropertyValue('--rat-height'));
 
@@ -19,7 +17,7 @@
         OPEN_AREA: 'open-area'
     };
 
-    Array.prototype.shuffle = function() {
+    Array.prototype.shuffle = function () {
         for (let i = this.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
             [this[i], this[j]] = [this[j], this[i]];
@@ -33,40 +31,45 @@
             this.illness = 3;
             this.index = index;
             this.infected = false;
-			if (!SILENT_MODE) {
-				this.img = document.createElement("img");
-				this.img.src = `game/rats/rat.png`; // `game/rats/${type}.png`;
-				document.getElementById('board').appendChild(this.img);
-			}
+            if (!SILENT_MODE) {
+                this.img = document.createElement("img");
+                this.img.src = `game/rats/rat.png`; // `game/rats/${type}.png`;
+                document.getElementById('board').appendChild(this.img);
+            }
         }
+
         setPos(pos) {
-			if (this.img) {
-				this.img.style.transform = `translate(${pos.x*RAT_WIDTH}px, ${pos.y*RAT_HEIGHT + 10}px)`;
-			}
+            if (this.img) {
+                this.img.style.transform = `translate(${pos.x * RAT_WIDTH}px, ${pos.y * RAT_HEIGHT + 10}px)`;
+            }
             return this;
         }
+
         setOpacity(opacity) {
-			if (this.img) {
-				this.img.style.opacity = opacity;
-			}
+            if (this.img) {
+                this.img.style.opacity = opacity;
+            }
             return this;
         }
+
         heal() {
             this.illness--;
         }
+
         isIll() {
             return this.illness > 0;
         }
+
         destroy() {
-			if (this.img) {
-				document.getElementById('board').removeChild(this.img);
-				this.img = null;
-			}
+            if (this.img) {
+                document.getElementById('board').removeChild(this.img);
+                this.img = null;
+            }
         }
     }
 
     class Street {
-        constructor() {
+        constructor(X, Y) {
             const ratTypes = new Array(100);
             ratTypes.fill(RAT.MANAGER, 0, 20);
             ratTypes.fill(RAT.EMPLOYEE, 20, 70);
@@ -77,29 +80,44 @@
                 .map((type, index) => new Rat(type, index));
 
             this.update();
+
+            this.X = X;
+            this.Y = Y;
+
         }
+
         update() {
-            this.rats.forEach(( rat, index ) => rat
-                .setPos({x: -index+11, y: 5})
-                .setOpacity( Math.max(0, 1 - index/10) )
+            this.rats.forEach((rat, index) => rat
+                .setPos({x: -index + 11 + this.X, y: this.Y})
+                .setOpacity(Math.max(0, 1 - index / 10))
             );
         }
-        pickFirstRat() {
+
+        pickRat() {
             const rat = this.rats.shift();
             this.update();
             return rat;
         }
+
+        isEmpty() {
+            return this.rats.length === 0;
+        }
+
         destroy() {
-            this.rats.forEach( rat => rat.destroy() );
+            this.rats.forEach(rat => rat.destroy());
         }
     }
 
     class Hall {
-        constructor() {
-            this.capacity = 5;
-            this.rats = new Array(this.capacity).fill(null);
-            this.X = 1;
-            this.Y = 1;
+        constructor(capacity, X, Y) {
+            this.capacity = capacity;
+            this.rats = new Array(capacity).fill(null);
+            this.X = X;
+            this.Y = Y;
+        }
+
+        isFull() {
+            return this.rats.every(r => !!r);
         }
 
         pushRat(rat) {
@@ -109,49 +127,62 @@
             }
         }
 
+        hasRat(index) {
+            return index > 0 && index < this.capacity && !!this.rats[index];
+        }
         pickRat(index) {
-            if (index > 0 && index < this.capacity) {
+            if (this.hasRat(index)) {
                 const pickedRat = this.rats[index];
                 this.rats[index] = null;
                 return pickedRat;
-            }
-            else return null;
+            } else return null;
         }
+
         destroy() {
-            this.rats.forEach( rat => rat.destroy() );
+            this.rats.forEach(rat => rat && rat.destroy());
         }
     }
 
     class Room {
-        constructor( capacity, X, Y ) {
-            this.rats = [];
+        constructor(capacity, X, Y) {
             this.capacity = capacity;
+            this.rats = new Array(capacity).fill(null);
             this.X = X;
             this.Y = Y;
             this.numOfInfetcions = 0;
         }
 
+        isFull() {
+            return this.rats.every(r => !!r);
+        }
+
         pushRat(rat) {
             const firstEmptyIndex = this.rats.findIndex(rat => rat === null);
             if (rat && firstEmptyIndex >= 0) {
                 this.rats[firstEmptyIndex] = rat.setPos({x: this.X + firstEmptyIndex, y: this.Y});
+                return true;
+            } else {
+                return false;
             }
         }
 
         update() {
+
+            const rats = this.rats.filter( rat => !!rat);
+
             // infecting
-            const numOfInfetedRats = this.rats.reduce(( sum, rat ) => sum += (rat.infected ? 1 : 0));
+            const numOfInfetedRats = rats.reduce((sum, rat) => sum += (rat.infected ? 1 : 0), 0);
             if (numOfInfetedRats > 0) {
                 this.rats.forEach(rat => {
                     if (!rat.infected) {
                         rat.infected = true;
-                        this.numOfInfetcions ++;
+                        this.numOfInfetcions++;
                     }
                 });
             }
 
             // healing
-            this.rats.forEach(( rat, index ) => {
+            rats.forEach((rat, index) => {
                 rat.heal();
                 if (!rat.isIll()) {
                     this.rats[index] = null;
@@ -161,7 +192,7 @@
         }
 
         destroy() {
-            this.rats.forEach( rat => rat.destroy() );
+            this.rats.forEach(rat => rat && rat.destroy());
         }
     }
 
@@ -173,8 +204,8 @@
             document.getElementById('pause').onclick = () => this.run(undefined);
             document.getElementById('reset').onclick = () => this.reset();
             document.getElementById('average').onclick = () => this.calculateAverage(1000);
-			
-            this.rules = [
+
+            this.rules = [ /*
                 () => this.rooms[ROOM.UNDERGROUND_GARAGE].rats
                     .reduce((sum, rat, position) => sum + (rat.type === RAT.MANAGER && position >= 20 ? 10 : 0), 0),
                 () => this.rooms[ROOM.RENO_COURT].rats
@@ -187,46 +218,80 @@
                         .reduce((sum, rat) => sum + (rat.type === RAT.EMPLOYEE ? 2 : 0), 0)
                     + this.rooms[ROOM.OPEN_AREA].rats
                         .reduce((sum, rat) => sum + (rat.type === RAT.EMPLOYEE ? 2 : 0), 0)
-            ];
+            */];
 
             this.reset();
         }
-		showProgress(label) {
-			document.getElementById('progress').style.display = !!label ? 'block' : 'none';
-			document.getElementById('progress').innerHTML = label;
-			SILENT_MODE = !!label;
-		}
-		calculateAverage( numOfRounds ) {
-			this.showProgress(`Calculating ${numOfRounds} rounds...`);
-			setTimeout( () => {
-				const sumScores = [0,0,0];
-				for (let n = 0; n < numOfRounds; n++) {
-					this.reset();
-					for (let m = 0; m < 100; m++) {
-						this.doAStep(true);
-					}
-					const scores = this.calculateScores();
-					scores.forEach( (score, index) => sumScores[index] += score );
-				}
-				const averageScores = sumScores.map ( sum => sum / numOfRounds );
-				this.showProgress(false);				
-				this.updateUI(averageScores, numOfRounds);
-			}, 0);
-		}
+
+        reset() {
+            if (this.street)
+                this.street.destroy();
+            if (this.hall)
+                this.hall.destroy();
+            if (this.rooms) {
+                this.rooms.forEach(room => room.destroy());
+            }
+
+            this.street = new Street(-6, 6);
+            try {
+                this.erManager = new ERManager();
+            } catch (e) {
+                console.error('Cannot create new ERManager', e);
+            }
+
+            this.hall = new Hall(5,8, 6);
+
+            this.rooms = [
+                new Room(1, 7, 4),
+                new Room(2, 10, 4),
+                new Room(3, 9, 2),
+            ];
+
+            this.speed = undefined;
+            this.updateUI([0, 0, 0]);
+        }
+
+        showProgress(label) {
+            document.getElementById('progress').style.display = !!label ? 'block' : 'none';
+            document.getElementById('progress').innerHTML = label;
+            SILENT_MODE = !!label;
+        }
+
+        calculateAverage(numOfRounds) {
+            this.showProgress(`Calculating ${numOfRounds} rounds...`);
+            setTimeout(() => {
+                const sumScores = [0, 0, 0];
+                for (let n = 0; n < numOfRounds; n++) {
+                    this.reset();
+                    for (let m = 0; m < 100; m++) {
+                        this.doAStep(true);
+                    }
+                    const scores = this.calculateScores();
+                    scores.forEach((score, index) => sumScores[index] += score);
+                }
+                const averageScores = sumScores.map(sum => sum / numOfRounds);
+                this.showProgress(false);
+                this.updateUI(averageScores, numOfRounds);
+            }, 0);
+        }
+
         run(speed) {
             const stopped = this.speed === undefined;
             this.speed = speed;
             if (stopped) {
                 this.doAStep();
             }
+
         }
-		calculateScores() {
-			return this.rules.map( rule => rule());
-		}
+
+        calculateScores() {
+            return this.rules.map(rule => rule());
+        }
+
         updateUI(scores, numOfRounds) {
             const total = scores.reduce((sum, score) => sum + score, 0);
- 
-			document.getElementById('rounds').innerHTML = numOfRounds ? `(${numOfRounds} rounds)` : '';
+
+            document.getElementById('rounds').innerHTML = numOfRounds ? `(${numOfRounds} rounds)` : '';
 
             document.getElementById('score1').innerHTML = numOfRounds ? parseFloat(scores[0]).toFixed(2) : scores[0];
             document.getElementById('score2').innerHTML = numOfRounds ? parseFloat(scores[1]).toFixed(2) : scores[1];
@@ -240,75 +305,79 @@
             document.getElementById('pause').disabled = !streetHasRats || this.speed === undefined;
             document.getElementById('reset').disabled = this.street.rats.length === 100;
         }
-        reset() {
-            if (this.street)
-                this.street.destroy();
-            if (this.rooms) {
-                for (let roomId in this.rooms) {
-                    this.rooms[roomId].destroy();
-                }
-            }
 
-            this.street = new Street();
-            try {
-                this.erManager = new ERManager();
-            } catch (e) {
-                console.error('Cannot create new ERManager', e);
-            }
 
-            this.rooms = {
-                [ROOM.UNDERGROUND_GARAGE]:
-                    new Room( 30, (index) => ({
-                        x: index % 10 + 1,
-                        y: 13 - Math.floor(index / 10)
-                    }) ),
-                [ROOM.RENO_COURT]:
-                    new Room( 30, (index) => ({
-                        x: Math.floor(index / 2) + 4,
-                        y: index % 2 + 2
-                    }) ),
-                [ROOM.OPEN_AREA]:
-                    new Room( 30, (index) => ({
-                        x: index % 5 + 14 ,
-                        y: Math.floor(index / 5) + 8
-                    }) )
-            };
 
-            this.speed = undefined;
-            this.updateUI([0,0,0]);
-        }
         doAStep() {
-            if ( SILENT_MODE || this.speed !== undefined) {
-                const nextRat = this.street.pickFirstRat();
-                if (nextRat) {
-                    let roomId;
-                    try {
-                        roomId = this.erManager.selectRoom(nextRat.type);
+            if (SILENT_MODE || this.speed !== undefined) {
+                try {
+                    const result = this.erManager.redirectRatToRoom();
 
-                        if (roomId && this.rooms.hasOwnProperty(roomId)) {
-                            if (this.rooms[roomId].pushRat(nextRat)) {
-                                // success
+                    if (result
+                        && result.rat >= 1 && result.rat <= 5
+                        && result.room >= 1 && result.room  <= 5) {
+
+                        console.log(`Redirecting rat #${result.rat} to room #${result.room}`);
+
+                        // STREET -> HALL
+
+                        {
+
+
+                            if (this.street.isEmpty()) {
+                                console.log(`No more rat in the street!`);
+                            }
+                            else if (this.hall.isFull()) {
+                                console.log(`Hall is full!`);
                             } else {
-                                nextRat.destroy();
+                                const rat = this.street.pickRat();
+                                this.hall.pushRat(rat);
                             }
-                            if (!SILENT_MODE && this.speed !== undefined) {
-                                setTimeout(() => this.doAStep(), this.speed);
-                            }
-                        } else {
-                            nextRat.destroy();
-                            console.error(`'${roomId}' is not a valid parking lot id`);
-                            this.reset();
                         }
 
-                    } catch (e) {
-                        console.error('selectRoom() crashed.', e);
+                        // HALL -> ROOM
+
+                        {
+                            const ratIndex = result.rat-1;
+                            const room = this.rooms[result.room-1];
+
+                            if (!this.hall.hasRat(ratIndex)) {
+                                console.log(`No rat is sitting at position #${result.rat}!`);
+                            }
+                            else if (room.isFull()) {
+                                console.log(`Room #${result.room} is full!`);
+                            } else {
+                                const rat = this.hall.pickRat(ratIndex);
+                                room.pushRat(rat);
+                            }
+                        }
+
+                        // ROOM -> OUT
+
+                        this.rooms.forEach(room => room.update());
+
+                        // NEXT STEP
+
+                        if (!SILENT_MODE && this.speed !== undefined) {
+                            setTimeout(() => this.doAStep(), this.speed);
+                        }
+                    } else if (result === null) {
+                        console.log('Skipping a step');
+                    } else {
+                        console.error(`${result} is not a valid response`);
                         this.reset();
                     }
-					if (!SILENT_MODE) {
-						this.updateUI( this.calculateScores() );
-					}
+
+
+                } catch (e) {
+                    console.error('erManager.redirectRatToRoom() crashed.', e);
+                    this.reset();
+                }
+                if (!SILENT_MODE) {
+                    this.updateUI(this.calculateScores());
                 }
             }
+
         }
     }
 
