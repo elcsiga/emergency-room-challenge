96f4f08798de17b6c627342e30c5bbe63f4c92de
Andras Kovacs <Andras.Kovacs4@ge.com>
2019-05-17 10:38:52 +0200

    ujj_laszlo

--- ERManager.js
+++ ERManager.js
@@ -11,11 +11,18 @@ class ERManager {
 
     //  For each game a new ERManager() object will be created.
 
+	
+
     constructor() {
-        console.log('Starting a new game...');
+        //console.log('Starting a new game...');
 
         // enable game logging
-        window.logRats = true;
+        //window.logRats = true;
+		window.isBlackA = false;
+		window.isBlackB = false;
+		window.TreatedWhite = 0;
+		window.TreatedBlack = 0;
+		
     }
 
     //  redirectRatToSurgery() will be called repetitively until there are rats in the hall.
@@ -38,14 +45,135 @@ class ERManager {
     //  You MUST always return a valid response. If there is no rat in the given
     //  position or the given surgery room is occupied, the rat will remain in the hall.
 
-    redirectRatToSurgery(report) {
+    
+	
+	redirectRatToSurgery(report) {
 
-        // As an example, here we send a random rat to a random Surgery:
-        // You can do it better...
+		var i;
+		
+		var candidate = 1000;
+		
+		var waitingRats = [];
+		var emptyChair = 1000;
+		for(i=0;i<5;i++){
+			if(report.ratsInTheHall[i] != null){
+				waitingRats.push({index:i,rat:report.ratsInTheHall[i]});
+			}
+			else{
+				emptyChair = i;
+			}
+		}
+		
+		waitingRats.sort(function(r1,r2){
+			var v1 = r1.rat.remainingTime;
+			var v2 = r2.rat.remainingTime;
+			var discriminationfactor = 0.4*(TreatedBlack-TreatedWhite);
+			if(r1.rat.isBlack){
+				v1 = v1 + discriminationfactor;
+			} else {
+				v1 = v1 - discriminationfactor;
+			}
+			
+			if(r2.rat.isBlack){
+				v2 = v2 + discriminationfactor;
+			} else {
+				v2 = v2 - discriminationfactor;
+			}
 
-        const rat = Math.floor(Math.random() * 5);
-        const surgery = Math.floor(Math.random() * 3);
-        return { rat, surgery };
+			return v1 - v2;
+			
+			});
+		
+		
+		var found = false;
+		var canInfect = false;
+		var infectSurgery = 1000;
+		for(i=0;i<waitingRats.length;i++){
+
+		
+			candidate = waitingRats[i].index;
+		
+			var freeSurgery = 1000;
+			var j;
+			for(j=0;j<3;j++){
+				if(report.timeUntilSurgeryWillBeFree[j] < 2){
+					
+					
+					if(
+						(
+							j==0 && report.timeUntilSurgeryWillBeFree[1] > 1 && isBlackB!=report.ratsInTheHall[candidate].isBlack
+						)
+							
+						||
+						
+						(
+							j==1&& report.timeUntilSurgeryWillBeFree[0] > 1 && isBlackA!=report.ratsInTheHall[candidate].isBlack
+						)
+					){
+						// purposefully infect a white rat if we already treated more white and otherwise the black would die
+						
+						if( report.ratsInTheHall[candidate].isBlack&& (TreatedWhite-TreatedBlack) > 3 && report.ratsInTheHall[candidate].remainingTime < 2){
+							canInfect = true;
+							infectSurgery = j;
+							break;
+						}
+						
+						continue;
+					}
+						else {
+						freeSurgery = j;
+						found = true;
+						break;
+					}
+				}
+			}
+			
+			if(freeSurgery<3 || canInfect){
+
+				break;
+			}
+		
+		}
+		
+        var rat = Math.floor(Math.random() * 5);
+        var surgery = Math.floor(Math.random() * 3);
+        
+		if(found){
+			
+			rat = candidate;
+			surgery = freeSurgery;
+			if(report.ratsInTheHall[rat].isBlack){
+				TreatedBlack++;
+			} else {
+				TreatedWhite++;
+			}
+		} else if ( canInfect ){
+			//console.log("Infecting a white rat purposefully");
+			rat = candidate;
+			surgery = infectSurgery;
+			if(report.ratsInTheHall[rat].isBlack){
+				TreatedBlack++;
+			} else {
+				TreatedWhite++;
+			}
+		} else if ( emptyChair < 5 ) {
+			//Skip
+			rat = emptyChair;
+		} else {
+			//random rat to occupied surgery
+			surgery = 2;
+					
+		}
+				
+		if(surgery == 0 && report.ratsInTheHall[rat] != null){
+				isBlackA = report.ratsInTheHall[rat].isBlack;
+		}
+		
+		if(surgery == 1 && report.ratsInTheHall[rat] != null){
+				isBlackB = report.ratsInTheHall[rat].isBlack;
+		}
+		
+		return { rat, surgery };
     }
 }
 
